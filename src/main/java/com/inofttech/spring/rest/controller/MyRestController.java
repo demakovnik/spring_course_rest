ed7a13751af078fcd66d1b1eb8b5075cb9646d55
demakovnik <demakoffnik@gmail.com>
2020-12-24 21:33:27 +0700

    Lesson 80. Get Employee List. Fix mistakes

--- src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
+++ src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
@@ -1,9 +1,30 @@
 package com.inofttech.spring.rest.controller;
 
+import com.inofttech.spring.rest.entity.Employee;
+import com.inofttech.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+
+
+
+
+
 }
