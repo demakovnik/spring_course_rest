bc8df2e83facd53efa59106ec2bbd15f2d0ba859
demakovnik <demakoffnik@gmail.com>
2020-12-24 23:36:02 +0700

    Lesson 82. Exception handling.

--- src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
+++ src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
@@ -1,12 +1,13 @@
 package com.inofttech.spring.rest.controller;
 
 import com.inofttech.spring.rest.entity.Employee;
+import com.inofttech.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.inofttech.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.inofttech.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,13 +26,21 @@ public class MyRestController {
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployeeById(id);
 
-        return employeeService.getEmployeeById(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID=" + id
+                    + " in database");
+        }
+        return employee;
     }
 
-
-
-
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
 
 
 }
---
+++ src/main/java/com/inofttech/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.inofttech.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException{
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
