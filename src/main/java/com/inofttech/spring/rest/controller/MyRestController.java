d9b695542188a482f7f0bcbf44f5e9fb7e44f29e
demakovnik <demakoffnik@gmail.com>
2020-12-24 22:06:12 +0700

    Lesson 81. Get one Employee.

--- src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
+++ src/main/java/com/inofttech/spring/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.inofttech.spring.rest.entity.Employee;
 import com.inofttech.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,12 @@ public class MyRestController {
     public List<Employee> showAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+
+        return employeeService.getEmployeeById(id);
     }
 
 
